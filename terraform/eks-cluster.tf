a8679d25bbbdc02fe90160b5d8b02ad95e6fb1f2
suhailzap <55779498+suhailzap@users.noreply.github.com>
2023-09-22 12:26:23 +0200

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,7 +5,7 @@ module "eks" {
   cluster_name    = "my-cluster"
   cluster_version = "1.27"
 
-    cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access  = true
 
     vpc_id = module.myapp-vpc.vpc_id
     subnet_ids = module.myapp-vpc.private_subnets
