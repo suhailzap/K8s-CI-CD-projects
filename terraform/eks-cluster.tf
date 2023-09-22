ebe2756eb6cbac8756667b8704f1f5d062ecf27c
suhailzap <55779498+suhailzap@users.noreply.github.com>
2023-09-22 12:23:09 +0200

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.16.0"
     cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.24"
+    cluster_version = "1.27"
 
     cluster_endpoint_public_access  = true
 
